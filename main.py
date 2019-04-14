dccd8eca1cc179daa4a9fd99442845c460f293e4
Krystine <krystinecarrington@gmail.com>
2019-04-14 02:16:52 -0700

    jewel breathes while sound plays. cleaned up code

--- main.py
+++ main.py
@@ -22,28 +22,34 @@ import adafruit_lis3dh
 #-------------------------------------------------------------------------------------------
 #                                          SETUP
 #-------------------------------------------------------------------------------------------
-WAV_FILE_NAME = "test_room.wav"                 # name of your wave file in top folder
-COLOR_WHITE = (255, 255, 255)                   # white light (or FFFFFF)
-COLOR_RED   = (255, 0, 0)                       # red light
-MOVE_THRESH = 100                               # threshold for determining if holocron moved
+WAV_FILE_NAME       = "test_room.wav"           # name of your wave file in top folder
+COLOR_WHITE         = (255, 255, 255)           # white light (or FFFFFF)
+COLOR_RED           = (255, 0, 0)               # red light
+MOVE_THRESH         = 100                       # threshold for determining if holocron moved
+NORM_BRIGHTNESS     = 0.5                       # brightness of LED while sound is off
+MAX_BRIGHTNESS      = 1.0                       # maximum brightness of Jewel
+MIN_BRIGHTNESS      = 0.1                       # chosen minimum brightness of Jewel
 
 """ Enable power to the Prop-Maker Wing """
-WING_PWR = digitalio.DigitalInOut(board.D10)
+WING_PWR = digitalio.DigitalInOut(board.D10)    # Pin D10 must be enabled for Prop-Maker Wing
 WING_PWR.direction = digitalio.Direction.OUTPUT
 WING_PWR.value = True
 
-""" built-in LED on the Feather, used for debug status """
-status = digitalio.DigitalInOut(board.D13)
-status.direction = digitalio.Direction.OUTPUT
-status.value = True
+""" Built-in LED on the Feather, used for debug status """
+status_led = digitalio.DigitalInOut(board.D13)
+status_led.direction = digitalio.Direction.OUTPUT
+status_led.value = True
+
+""" Set up built-in NeoPixel on Feather M0 Express """
+PIX = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1, auto_write=True)
+PIX.fill(COLOR_RED)                             # set color to red
 
 """ Set up NeoPixel Jewel """
 NUM_PIXELS = 14                                 # NeoPixel length (7 doesn't work for me?)
 NEOPIXEL_PIN = board.D5                         # pin where Jewel is connected (D5 for this wing)
-JEWEL = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS, brightness=0.1)
+JEWEL = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS, brightness=0.1, auto_write=True)
 
 JEWEL.fill(COLOR_WHITE)                         # turn on all NeoPixels in the jewel
-JEWEL.show()
 
 """ Set up accelerometer """
 I2C = busio.I2C(board.SCL, board.SDA)
@@ -51,29 +57,13 @@ ACCEL = adafruit_lis3dh.LIS3DH_I2C(I2C, address=0x18)
 ACCEL.range = adafruit_lis3dh.RANGE_4_G
 
 #-------------------------------------------------------------------------------------------
-#                                        FUNCTIONS
-#-------------------------------------------------------------------------------------------
-def play_wav(wav_name):
-    """
-    Play a .wav file from the top directory (the same directory as the main.py file).
-    
-    @param wav_name : Name of .wav file defined above. must include ".wav" extension.
-    @param loop     : If True, sound will repeat indefinitely. Default is false.
-    """
-    try:
-        wave_file = open(wav_name, "rb")
-        wave = audioio.WaveFile(wave_file)
-        AUDIO.play(wave, loop=False)
-    except:
-        return
- 
-#-------------------------------------------------------------------------------------------
 #                                       MAIN LOOP
 #-------------------------------------------------------------------------------------------
 while True:
-    status.value = True
+    """ Toggle status LED on Feather. Comment this part out after debugging. """
+    status_led.value = True
     time.sleep(0.5)
-    status.value = False
+    status_led.value = False
     time.sleep(0.5) 
     
     """ Get accelerometer reading and do calculation """
@@ -88,11 +78,37 @@ while True:
             wave_file = open(WAV_FILE_NAME, "rb")           # open .wav
             wave = audioio.WaveFile(wave_file)
          
-            AUDIO.play(wave)
-            JEWEL.fill(COLOR_RED)                           # set red color
-            JEWEL.show()
+            AUDIO.play(wave)                                # start audio
+            
+            """ While audio plays, animate a breathing effect on the Jewel"""
+            i = NORM_BRIGHTNESS * 100                       # convert to number 0-100 instead of fractional number
+            
             while AUDIO.playing:
-                pass
+                """ Increase in brightness to max. Break early
+                    if the audio finishes playing """
+                while i < (MAX_BRIGHTNESS*100) and AUDIO.playing:
+                    JEWEL.brightness = (i / 100.0)
+                    i += 1
+                    
+                time.sleep(0.25)
                 
-        wave_file.close()                                   # close .wav so we can play it again later :)
-        JEWEL.fill(COLOR_WHITE)                             # set color back to solid white
+                """ Decrease in brightness to min. Break early
+                    if the audio finishes playing """                
+                while i > (MIN_BRIGHTNESS*100) and AUDIO.playing:
+                    JEWEL.brightness = (i / 100.0)  
+                    i -= 1
+                    time.sleep(0.01)
+                
+            wave_file.close()                               # close .wav so we can play it again later :)
+            
+            """ Smoothly return Jewel back to its normal brightness. """
+            if i > (NORM_BRIGHTNESS*100):
+                while i > (NORM_BRIGHTNESS*100):
+                    JEWEL.brightness = (i / 100.0)
+                    i -= 1
+            elif i < ((NORM_BRIGHTNESS*100)):
+                while i < (NORM_BRIGHTNESS*100):
+                    JEWEL.brightness = (i / 100.0)
+                    i += 1
+            else:
+                JEWEL.brightness = NORM_BRIGHTNESS
