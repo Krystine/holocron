77534dec5cc35281659cd8553b9b139cea096332
Krystine <krystinecarrington@gmail.com>
2019-04-14 02:23:07 -0700

    added GNU general public license info

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-""" Darth Azaryia's Sith Holocron """
+""" Darth Azaryia's Sith Holocron 
+    Copyright (C) 2019  Krystine Carrington
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. """
 
 """ Designed to be deployed on a Feather M0 Express with the 
     following additional hardware:
